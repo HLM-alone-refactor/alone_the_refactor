5b354710c6c5a731373d3c7dbbc989fd6722aefc
gtulkn12 <45664554+gtulkn12@users.noreply.github.com>
2020-09-18 08:54:54 -0700

    Added Rainstorm encounter

--- src/main/java/com/palehorsestudios/alone/dayencounter/RainStormDay.java
+++ src/main/java/com/palehorsestudios/alone/dayencounter/RainStormDay.java
@@ -1,20 +1,19 @@
-package com.palehorsestudios.alone.nightencounter;
+package com.palehorsestudios.alone.dayencounter;
 
-import com.palehorsestudios.alone.Foods.Food;
-import com.palehorsestudios.alone.Foods.FoodFactory;
 import com.palehorsestudios.alone.Items.Item;
 import com.palehorsestudios.alone.Items.ItemFactory;
+import com.palehorsestudios.alone.dayencounter.DayEncounter;
 import com.palehorsestudios.alone.player.Player;
 
-public class RainStorm extends NightEncounter {
-    private static NightEncounter encounter;
+public class RainStormDay extends DayEncounter {
+    private static DayEncounter encounter;
 
-    private RainStorm() {
+    private RainStormDay() {
     }
 
-    public static NightEncounter getInstance() {
+    public static DayEncounter getInstance() {
         if (encounter == null) {
-            encounter = new RainStorm();
+            encounter = new RainStormDay();
         }
         return encounter;
     }
@@ -71,19 +70,7 @@ public class RainStorm extends NightEncounter {
             player.getItems().clear();
             // description for if the player does not have anything to shelter his items outside his shelter
             if (player.isDead()) {
-                return "Rain storm without items to protect equipment " + "The rainbow had been swallowed by a +" +
-                "rushing thundercloud. The sun darkened and for a brief moment +" +
-                        "raindrops fell on the woods. \n" +
-                        "You see the first lightning descend in the distance, and the thunder +" +
-                        "rumbled a short time later and was reinforced by the multiple echo +" +
-                        "throughout the woods. A second flash of light flashed through the clouds +" +
-                        "and hit somewhere on the other side of the woods. The thunder rumbled again, +" +
-                        "echoing again. \n" +
-                        "The storm came closer. A flash of light illuminated the sky and the rain +" +
-                        "increased significantly. Previously only a few drops had fallen down here and +" +
-                        "there. Now the rain pelted down so vehemently that that you could no +" +
-                        "longer see in front of you.  The rain is getting stronger, and with +" +
-                        "those flashes, it's not good to sit under a tree! \n";
+                return "Rain storm without items to protect equipment ";
             }
             return "rain" + "Rain Storm with Backpack" +
                     "The rainbow had been swallowed by a +" +
